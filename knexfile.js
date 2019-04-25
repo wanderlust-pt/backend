7cd8316f41c3c1c1f000f1d830cb7538027e2000
Jesse Theisen <theiseja@gmail.com>
2019-04-24 21:17:20 -0500

    Started writing register endpoint & getting knex setup

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,45 @@
 // Update with your config settings.
+require('dotenv').config();
+const localPg = {
+  host: 'localhost',
+  database: 'wunderlist 2.0',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+};
+
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './database/wunderlist.sqlite3'
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     }
   },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: dbConnection,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   }
-
-};
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
 const database = require('../database/dbConfig');
 
+// Token generation, cookie
+const { genToken } = require('../database/middleware/auth');
+
+
+// POST register account
+router.post('/register', async (req, res) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    try {
+        const [id] = await database('users').insert(user);
+        res.status(201).json({
+            message: "New user created",
+            id
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "Error creating user"
+        })
+    }
+});
+
 module.exports = router;
