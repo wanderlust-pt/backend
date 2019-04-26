f5e3b3584f86d28a205970319d04c705bd64d6fc
Jesse Theisen <theiseja@gmail.com>
2019-04-25 20:54:41 -0500

    Started writing CRUD endpoints

--- database/migrations/20190424212758_createTodosTable.js
+++ database/migrations/20190424212758_createTodosTable.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('todos', table => {
+      table.increments(); // id
+      table.string('title', 128) // title limited to 128 characters
+        .notNullable(); // title has to be filled
+      table.string('task', 256); // task limited to 256 characters
+      table.string('notes', 256); // extra notes limited to 256 characters
+      table.date('setDate'); // set date reminder
+      table.integer('userId')
+            .unsigned()
+            .references('id')
+            .inTable('users'); // user id that the note belongs to
+      table.boolean('completed'); // checks if todo is completed with a yes or no
+      table.timestamps(true, true); // timestamps
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('todos');
 };
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 const database = require('../database/dbConfig');
 
+// GET all todos
+router.get('/todos', async (req, res) => {
+    try {
+        console.log(req.decoded.subject)
+        const todo = await database('todos').where('userId', req.decoded.subject);
+        res.status(200).json(todo)
+    } catch (error) {
+        res.status(500).json({
+            error: "The todo's information could not be retrieved"
+        })
+    }
+});
+
 module.exports = router;
