05e27a21db913e2dc6c5c8c13a07ec9a81232dfa
Jesse Theisen <theiseja@gmail.com>
2019-05-01 22:27:12 -0500

    Cleaned up code a bit

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const localPg = {
   host: 'localhost',
-  database: 'wunderlist 2.0',
+  database: 'wunderlist',
   user: process.env.DB_USER,
   password: process.env.DB_PASS
 };
